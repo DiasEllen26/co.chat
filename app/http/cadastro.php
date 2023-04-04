2d03905ce2ccf13089e40bb53cb14ebedaaae65c
DiasEllen26 <ellen.c.s.dias@outlook.com.br>
2023-04-04 12:12:25 -0300

    Criando banco de dados e conexão

--- app/http/cadastro.php
+++ app/http/cadastro.php
@@ -25,7 +25,63 @@
                 $erro = "Senha é obrigatório";
                 header("Location: ../../cadastro.php?error=$erro&$data");
                 exit;
-                } else{
-                header("Location: ../../cadastro.php");
-            }
+                }
+                {
+                    $sql = "SELECT username FROM users WHERE username=?";
+                    $stmt = $con->prepare($sql);
+                    $stmt->execute([$username]);
+
+                    if ($stmt->rowCount() > 0) {
+                        $erro = "O username ($username) está indisponível";
+                        header("Locattion: ../../cadastro.php?error=$erro&$data");
+                        exit;
+                    } else {
+                        // foto de perfil carregada
+                        if (isset($_FILES['avatar'])) {
+                            // obter dados e armazenando em variavel 
+                            $img_nome = $_FILES['avatar']['nome'];
+                            $tmp_nome = $_FILES['avatar']['tmp_nome'];
+                            $error = $_FILES['avatar']['error'];
+                            if($error === 0) {
+                                // obter extensões de imagem armazená-lo em var
+                                $img_extencao = pathinfo($img_nome, PATHINFO_EXTENSION);
+                                //coverter a imagem em minusculo e converte em var
+                                $img_extencao_min = strtolower($img_extencao);
+                                //criando array que armazena permissão para upload de extensão de imagem
+                                $img_upload = array("jpg", "jpeg", "png");
+                                //verificando se a extensão da imagem está presente no array $img_upload
+                                if(in_array($img_extencao, $img_upload)) {
+                                    $nova_img_nome = $username.'.'.$img_extencao_min;
+                                    $img_upload_pasta = '../../upload/'.$nova_img_nome;
+                                    //move a imagem carregada para a pasta ./upload
+                                    move_uploaded_file($tmp_nome, $img_upload_pasta);
+                                }else{
+                                    $erro = "Esse arquivo não foi possível carregar.";
+                                    header("Locattion: ../../cadastro.php?error=$erro&$data");
+                                    exit;
+                                }
+                        }else{
+                            $erro = "Erro desconhecido!";
+                            header("Locattion: ../../cadastro.php?error=$erro&$data");
+                            exit;
+                            }
+                        }
+                    }
+                } 
+                $senha = password_hash($senha, PASSWORD_DEFAULT);
+                if (isset($nova_img_nome)){
+                    $sql = "INSET INTO users(nome, username, senha, avatar) VALUES (?,?,?,?)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute([$nome, $username, $senha, $nova_img_nome]);
+                }else {
+                    $sql = "INSERT INTO users (nome, username, senha) VALUES (?,?,?)";
+                    $stmt->execute([$nome, $username, $senha]);
+                }
+                // mensagem de registro
+                $registrado = "Conta criada com sucesso!";
+
+                header("Location: ../../index.php?success=$sm");
+                exit;
+        } else{
+            header("Location: ../../cadastro.php");
         }
--- app/http/config.php
+++ app/http/config.php
@@ -2,11 +2,11 @@
     $servidor = "localhost";
     $usuario = "root";
     $senha = "root";
-    $banco = "co.chat";
+    $banco = "cochat";
 
     try {
-        $con = new PDO("mysql:hot=$servidor;dbname=$co.chat,$usuario,$senha");
+        $con = new PDO("mysql:host={$servidor};dbname={$banco};port=3386;charset=utf8;",$usuario,$senha);
         $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);}
         catch(PDOException $e){
-            echo "Sem conexão com banco de dados > " . $e->getMessage();
+            echo "Sem conexão com banco de dados > ".$e->getMessage();
         }
